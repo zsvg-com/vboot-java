47676f72ab5e66a855e8d7ea5e2e3e16410b9652
zsvg <8680782@qq.com>
2021-12-15 18:35:17 +0800

    增加流程分类与模板

--- vboot-extend-wf/src/main/java/com/zsvg/vboot/wf/tem/cate/WfTemCate.java
+++ vboot-extend-wf/src/main/java/com/zsvg/vboot/wf/tem/cate/WfTemCate.java
@@ -1,13 +1,13 @@
 package com.zsvg.vboot.wf.tem.cate;
 
 import com.zsvg.vboot.common.mvc.entity.BaseCateEntity;
+import com.zsvg.vboot.module.sys.auth.menu.SysAuthMenu;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,6 +17,9 @@ public class WfTemCate extends BaseCateEntity {
     @ManyToOne(fetch= FetchType.EAGER)
     @JoinColumn(name = "pid")
     private WfTemCate parent;//父类别
+
+    @Transient
+    private List<WfTemCate> children = new ArrayList<>(); //行项目
 
     private String notes;//备注
 }
--- vboot-extend-wf/src/main/java/com/zsvg/vboot/wf/tem/cate/WfTemCateApi.java
+++ vboot-extend-wf/src/main/java/com/zsvg/vboot/wf/tem/cate/WfTemCateApi.java
@@ -3,9 +3,12 @@ package com.zsvg.vboot.wf.tem.cate;
 import com.zsvg.vboot.common.mvc.api.RestResult;
 import com.zsvg.vboot.common.mvc.dao.Sqler;
 import com.zsvg.vboot.common.mvc.pojo.Ztree;
+import com.zsvg.vboot.common.util.lang.XstringUtil;
+import com.zsvg.vboot.module.sys.auth.menu.SysAuthMenu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,9 +20,10 @@ public class WfTemCateApi {
     @GetMapping
     public RestResult get(String name) {
         Sqler sqler = new Sqler(table);
+        sqler.addSelect("t.pid,t.crtim,t.uptim");
         sqler.addLike("t.name", name);
-        sqler.addSelect("t.notes,t.ornum");
-        return RestResult.ok(service.findPageData(sqler));
+        sqler.addOrder("t.ornum");
+        return RestResult.ok(service.findTree(sqler));
     }
 
     @GetMapping("one/{id}")
